782f191fbaaa1e17f984081f47960badab155fd3
Cloud Cheng <annie22336699@hotmail.com>
2022-04-06 20:55:11 +0800

    詳細頁取名完成

--- routes/customize.js
+++ routes/customize.js
@@ -2,6 +2,7 @@ const express = require("express");
 const db = require("./../modules/connect-db");
 const router = express.Router();
 
+// 取得材料資料轉json
 async function getMtls(){
     const output = {
         success: false,
@@ -23,7 +24,7 @@ async function getMtls(){
       return output;
 }
 
-
+// 把資料傳進SQL
 async function stockpile(req){
   const output = {
     success: false,
@@ -140,6 +141,79 @@ async function stockpile(req){
   }
 }
 
+// 撈出資料
+async function getCusProd(req){
+  const output = {
+    success: false,
+    rows: [],
+  };
+
+  const memid=req.body.memid ?? 0;
+
+  const CusProd_sql = `SELECT * FROM cm_prod WHERE mem_id=${memid} AND cm_prod_status='undone'`;
+  // const test = `SELECT cm_prod.*, cm_prod_mtl.*, mtl.mtl_sugar, mtl.mtl_oil, mtl.mtl_protein, mtl.mtl_carbo, mtl.mtl_na, mtl.mtl_calories
+  // FROM cm_prod
+  // LEFT JOIN cm_prod_mtl
+  // ON cm_prod_mtl.cm_prod_id=cm_prod.cm_prod_id
+  // LEFT JOIN mtl
+  // ON cm_prod_mtl.mtl_id=mtl.mtl_id`;
+  const [rs] = await db.query(CusProd_sql); 
+  
+  if(rs.length === 0){
+    output.message = 'Product is not found';
+    return output;
+}
+
+  output.success = true;
+  output.rows = rs;
+  return output;
+}
+
+// 取得做好的材料
+async function getCusMtls(){
+  const output = {
+      success: false,
+      rows: [],
+    };
+  // const t_sql = `SELECT * FROM mtl WHERE 1`;
+  // const [rs] = await db.query(t_sql); 
+
+  // if(rs.length === 0){
+  //     output.message = 'MtlData not found';
+  //     return output;
+  // }
+
+  //   output.success = true;
+  //   output.rows = rs;
+  //   return output;
+}
+
+
+// 把資料傳進SQL
+async function addname(req){
+  const output = {
+    success: false,
+    message: '',
+    rows: [],
+  };
+
+  const memid=req.body.memid ?? 0;
+  const cusProdName=req.body.cus_name ?? 0;
+
+  console.log('memid:', memid, 'cusProdName:', cusProdName)
+
+  // 確認儲存狀態
+  const sql= `SELECT * FROM cm_prod WHERE mem_id=${memid} AND cm_prod_status='undone'`;
+  const [info]=await db.query(sql);
+
+  // 修改
+  const editCusDataSQL = `UPDATE cm_prod SET cm_prod_status='completed',cm_prod_name=? WHERE mem_id=? AND cm_prod_status='undone'`;
+  const [editCusData]= await db.query(editCusDataSQL, [
+    cusProdName,
+    memid, 
+  ])
+}
+
 //取得所有mtl資料
 router.get("/api/getmtls", async (req, res) => {
   res.json(await getMtls());
@@ -148,12 +222,20 @@ router.get("/api/getmtls", async (req, res) => {
 // 接收看看
 router.post("/api/postcusdata", async (req, res) => {
   res.json(req.body);
-  // console.log('接收的東西：',req.body);
-
   stockpile(req);
 })
 
+// 取得該會員做好的品項
+router.post("/api/getcusprod", async (req, res) => {
+  res.json(await getCusProd(req));
+});
+
+router.post("/api/postfinaldata", async (req, res) => {
+  res.json(req.body);
+  addname(req);
+});
+
 //加進購物車
-addToCart(data.memid,sendListToSetOrder.insertId,data.combo,data.combosum/data.combo,0,'set')
+// addToCart(data.memid,sendListToSetOrder.insertId,data.combo,data.combosum/data.combo,0,'set')
 
 module.exports = router;
